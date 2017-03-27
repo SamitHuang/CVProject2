5315f163ec2c78434f2eb8e0d256535e0d8aee76
zhoubo01 <zhoubo01@baidu.com>
2017-03-27 16:54:45 +0800

    get input data changed

--- GetInputData.py
+++ GetInputData.py
@@ -4,14 +4,14 @@ import os
 import numpy as np
 import random
 import cv2
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from PIL import Image, ImageEnhance, ImageFilter
 import tensorflow as tf
 
 #data setting
 TRAIN_DATA_DIR='../data/train/'
-NUM_TRAIN =320
-NUM_TEST = 32
+NUM_TRAIN = 2400
+NUM_TEST = 500
 NUM_TRAIN_DOGS = NUM_TRAIN/2
 NUM_TRAIN_CATS = NUM_TRAIN/2
 NUM_TEST_DOGS=NUM_TEST/2
